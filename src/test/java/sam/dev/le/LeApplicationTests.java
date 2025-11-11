7cf5ad63aafd212f8e548dde3f1aef92106c3930
Sam-Zelcer <samzelcerr@gmail.com>
2025-11-11 13:12:54 +0100

    initial commit

---
+++ src/test/java/sam/dev/le/LeApplicationTests.java
@@ -0,0 +1,13 @@
+package sam.dev.le;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
